0901c9faaac44f7d1417856dfacbd1ab5d9d852e
ndgupta06 <patkatcourse2014@gmail.com>
2017-07-02 11:57:06 -0400

    MavenGitDemo - Initial check-in of MavengitDemo.

---
+++ MavenGitDemo/src/test/java/com/apk/mavengitdemo/Testscripts/NewTest.java
@@ -0,0 +1,31 @@
+package com.apk.mavengitdemo.Testscripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class NewTest {
+	
+	private WebDriver driver;		
+	@Test				
+	public void testEasy() {	
+		driver.get("http://demo.guru99.com/selenium/guru99home/");  
+		String title = driver.getTitle();				 
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+	}	
+	@BeforeTest
+	public void beforeTest() {	
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumChrome\\chromedriver.exe");
+		driver = new ChromeDriver();  
+	}		
+	@AfterTest
+	public void afterTest() {
+		driver.quit();			
+	}	
+	
+
+}
