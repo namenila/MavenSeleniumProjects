7d3677be71cae293cf1de3994ac6649b765b13bc
ndgupta06 <patkatcourse2014@gmail.com>
2017-07-02 17:52:06 -0400

    2. Added sysout.

--- MavenGitDemo/src/test/java/com/apk/mavengitdemo/Testscripts/NewTest.java
+++ MavenGitDemo/src/test/java/com/apk/mavengitdemo/Testscripts/NewTest.java
@@ -15,7 +15,8 @@ public class NewTest {
 	public void testEasy() {	
 		driver.get("http://demo.guru99.com/selenium/guru99home/");  
 		String title = driver.getTitle();				 
-		Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
+		Assert.assertTrue(title.contains("Demo Guru99 Page")); 
+		System.out.println("WebSite title verified.");
 	}	
 	@BeforeTest
 	public void beforeTest() {	
